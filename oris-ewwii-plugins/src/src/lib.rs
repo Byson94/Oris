14e6221c44d132b9ee6d1ead807d81bd05822871
Byson94 <byson94@outlook.com>
2025-10-20 22:18:30 +0530

    refactor: a lot and lot of change that idk how to explain

--- oris-ewwii-plugins/src/src/lib.rs
+++ oris-ewwii-plugins/src/src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod app_search;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use ewwii_plugin_api::{EwwiiAPI, Plugin, export_plugin, rhai};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct OrisEwwiiPlugins;
+
+impl Plugin for OrisEwwiiPlugins {
+    fn init(&self, host: &dyn EwwiiAPI) {
+        use crate::app_search::app_search;
+
+        host.log("Checkpoint 1");
+    
+        host.register_function("get_place".to_string(), Box::new(|args| {
+            println!("ARGS: {:#?}", args);
+
+            rhai::Dynamic::from("yo")
+        }));
+
+        host.log("Checkpoint 2");
     }
 }
+
+export_plugin!(OrisEwwiiPlugins);
--- src/share/oris/ewwii/launch.sh
+++ src/share/oris/ewwii/launch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # or with the correct path
-CFG=/usr/share/oris/bar/
+CFG=/usr/share/oris/ewwii/
 
 ewwii daemon --config "$CFG" &
 ewwii open bar --config "$CFG"
+ewwii open time --config "$CFG"
